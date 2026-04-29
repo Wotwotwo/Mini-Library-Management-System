7bb3cf8e477aa3f7261f0420b29d075bcbb0f197
jmarielah <hernandezjeanniemariel@gmail.com>
2026-04-30 02:46:35 +0800

    updated log in

--- login.php
+++ login.php
@@ -3,23 +3,23 @@ session_start();
 include 'database.php';
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $email = $_POST['email']
-    $password = $_POST['password']
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
 
-    $sql = "SELECT * FROM members WHERE username = ?";
+    $sql = "SELECT * FROM members WHERE email = ?";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $email)
+    $stmt->bind_param("s", $email);
     $stmt->execute();
     $result = $stmt->get_result();
 
-    if($result->num-rows > 0){
+    if($result->num_rows > 0){
         $row = $result->fetch_assoc();
 
         if(password_verify($password, $row['password'])){
             
-            $_SESSION['user_id'] = $row['u_id'];
-            $_SESSION['admin'] = $row['a_id'];
+            $_SESSION['member_id'] = $row['m_id'];
+            $_SESSION['role'] = $row['role'];
 
             if($row['role'] == "admin") {
                 header("Location: admin_dashboard.php");
@@ -29,11 +29,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 exit();
             }
         }else {
-                echo "Error"
+                echo "Error";
             }
     }else {
-        echo "Invalid username or password"
+        echo "Invalid username or password";
     }
 }
 
 $conn->close();
+?>
